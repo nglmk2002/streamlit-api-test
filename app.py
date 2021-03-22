3440ab1b751dfd46dd73c0a64e47814441f6e58a
nglmk2002 <80315785+nglmk2002@users.noreply.github.com>
2021-03-22 09:51:24 +0900

    yfinace

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+#야후금융에서 주식정보를 제공하는 라이브러리 yfiance 이용
+#주식정보를 불러오고 차트 그리는거 합니다.
+
+#해당 주식에대한 트윗글들을 불러올수 있는 api가 있다.
+#stocktwits.com 에서 제공하는 Restful API 를 호출해서
+#데이터 가져오는것 실습
+
+def main():
+    pass
+
+if __name__ == '__main__' :
+    main()
